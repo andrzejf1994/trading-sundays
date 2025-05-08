7fca187c3895a80f6446441e3bd87aed2bb9f208
andrzejf1994 <andrzejf1994+github@gmail.com>
2025-05-08 12:37:14 +0200

    Fix binary_sensor import and device_class

--- binary_sensor.py
+++ binary_sensor.py
@@ -1,5 +1,5 @@
 from datetime import date
-from homeassistant.components.binary_sensor import BinarySensorEntity, DEVICE_CLASS_RUNNING
+from homeassistant.components.binary_sensor import BinarySensorEntity
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .const import DOMAIN
@@ -7,13 +7,12 @@ from .coordinator import TradingSundaysCoordinator
 
 async def async_setup_entry(hass, entry, async_add_entities):
     coordinator: TradingSundaysCoordinator = hass.data[DOMAIN][entry.entry_id]
-    async_add_entities([TradingSundayTodayBinarySensor(coordinator)], False)
+    async_add_entities([TradingSundayTodayBinarySensor(coordinator)], True)
 
 class TradingSundayTodayBinarySensor(CoordinatorEntity, BinarySensorEntity):
     """Binarny sensor: czy dziś jest niedziela handlowa?"""
 
     _attr_name = "Is Trading Sunday Today"
-    _attr_device_class = DEVICE_CLASS_RUNNING
     _attr_unique_id = f"{DOMAIN}_today"
 
     def __init__(self, coordinator: TradingSundaysCoordinator):
